b23c9d57e131eb644189f9693f7e1f4fcf688186
Brian Brazil <brian.brazil@robustperception.io>
2020-02-29 09:14:16 +0000

    Make homepage look better

--- wsmux/web.go
+++ wsmux/web.go
@@ -41,22 +41,61 @@ func homepage(w http.ResponseWriter, r *http.Request, wsMux *WSMux, externalURL 
 		return inactive[i].LastUpdated.After(inactive[j].LastUpdated)
 	})
 
-	fmt.Fprintf(w, `
+	fmt.Fprintf(w, "%s", `
   <html>
-  <head><title>Live Derby Stats</title></head>
+  <head>
+  <title>Live Derby Stats</title>
+  <style>
+  body {
+    background-color: #c1946a;
+    color: #dddddd;
+    margin: 0;
+    font-family: sans-serif;
+  }
+
+  .heading {
+    font-family: serif;
+    text-align: center;
+    color: #82b74b;
+    background-color: #3e4444;
+  }
+  h1 {
+    margin-bottom: 0;
+    font-size: 300%;
+  }
+  .subtitle {
+    font-size: 110%;
+    font-style: italic;
+  }
+
+  .main {
+    margin: 0;
+    padding: 2ex;
+    background-color: #405d27;
+    margin-left: 10%;
+    margin-right: 10%;
+  }
+  .main a {
+    color: #eeeeee;
+  }
+  .main table a {
+    font-weight: bolder;
+  }
+
+  .footer {
+    font-size: 70%;
+    text-align: center;
+  }
+  </style>
+  </head>
   <body>
-  <h1>Live Derby Stats</h1>
+  <div class="heading">
+  <h1>DerbyStats: Live</h1>
+  <div class="subtitle">Roller Derby Scoreboards in Real Time</div>
+  </div>
+  <div class="main">
   `)
-	if len(active) == 0 {
-		fmt.Fprintf(w, `
-    <p>There are currently no active scoreboards :(<p>
-    `)
-	} else {
-		if len(active) == 1 {
-			fmt.Fprintf(w, `<p>There is currently 1 active scoreboard:</p>`)
-		} else {
-			fmt.Fprintf(w, `<p>There are currently %d active scoreboards:</p>`, len(active))
-		}
+	if len(active) > 0 {
 		fmt.Fprintf(w, `
     <table border=1 cellpadding="3em" cellspacing="0">
     <tr><th>Name</th><th>Viewers</th><th>Summary</th><th>Age</th>
@@ -77,30 +116,42 @@ func homepage(w http.ResponseWriter, r *http.Request, wsMux *WSMux, externalURL 
 				now.Sub(l.LastUpdated).Round(time.Second*10).String())
 		}
 		fmt.Fprintf(w, `</table>`)
-	}
-
-	fmt.Fprintf(w, `
-    <p>There are stats from %d inactive scoreboards:<p>
-    <table border=1 cellpadding="3em" cellspacing="0">
-    <tr><th>Name</th><th>Summary</th><th>Age</th>`, len(inactive))
-	for _, l := range inactive {
-		u := *externalURL
-		u.Host = l.Name + "." + u.Host
-		age := now.Sub(l.LastUpdated).Round(time.Minute).String()
 		fmt.Fprintf(w, `
-    <tr>
-    <td><a href="%s">%s</td>
-    <td>%s</td>
-    <td style="text-align: right;">%s</td>
-    </tr>`,
-			html.EscapeString(u.String()), html.EscapeString(l.Name),
-			html.EscapeString(l.Summary),
-			strings.TrimSuffix(age, "0s"))
+    `)
+	} else {
+		fmt.Fprintf(w, `<p>There are currently no active scoreboards.</p>`)
+	}
+	fmt.Fprintf(w,
+		`<p>To appear here, ensure the scoreboard computer can access the internet, <a
+  href="https://github.com/DerbyStats/wsproxy/releases">download the wsproxy-binaries.zip</a> to the computer running
+  your scoreboard, extract it, and run the appropriate binary (Darwin = Mac OS X). You can be up and running in under a minute!</p>
+  `)
+
+	if len(inactive) != 0 {
+		fmt.Fprintf(w, `
+    <p>You can also view inactive scoreboards that have not recently sent updates:<p>
+    <table border=1 cellpadding="3em" cellspacing="0">
+    <tr><th>Name</th><th>Summary</th><th>Age</th>`)
+		for _, l := range inactive {
+			u := *externalURL
+			u.Host = l.Name + "." + u.Host
+			age := now.Sub(l.LastUpdated).Round(time.Minute).String()
+			fmt.Fprintf(w, `
+      <tr>
+      <td><a href="%s/views/standard/">%s</td>
+      <td>%s</td>
+      <td style="text-align: right;">%s</td>
+      </tr>`,
+				html.EscapeString(u.String()), html.EscapeString(l.Name),
+				html.EscapeString(l.Summary),
+				strings.TrimSuffix(age, "0s"))
+		}
 	}
 
 	fmt.Fprintf(w, `
   </table>
-  <p><a href="https://github.com/DerbyStats/wsproxy">Source Code</a></p>
+  <div class="footer"><a href="https://github.com/DerbyStats/wsproxy">Source Code</a></div>
+  </div>
   </body>
   </html>`)
 }
