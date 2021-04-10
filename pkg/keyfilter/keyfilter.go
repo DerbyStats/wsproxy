460d785c90e727b9edd6ed53e64329740176ceab
Brian Brazil <brian.brazil@robustperception.io>
2021-04-10 15:08:41 +0100

    blacklist->rejectlist

--- pkg/keyfilter/keyfilter.go
+++ pkg/keyfilter/keyfilter.go
@@ -7,23 +7,23 @@ import (
 )
 
 type KeyFilter struct {
-	blacklist *regexp.Regexp
+	rejectlist *regexp.Regexp
 }
 
 // New creates a new KeyFilter, which will reject keys matching the given regexes.
 // Regexes are anchored at the start.
-func New(blacklist []string) (*KeyFilter, error) {
-	if len(blacklist) == 0 || len(blacklist) == 1 && blacklist[0] == "" {
+func New(rejectlist []string) (*KeyFilter, error) {
+	if len(rejectlist) == 0 || len(rejectlist) == 1 && rejectlist[0] == "" {
 		return &KeyFilter{}, nil
 	}
 	// Combine into one regex for efficiency, but check individual regexs make sense first.
-	for _, s := range blacklist {
+	for _, s := range rejectlist {
 		if _, err := regexp.Compile(s); err != nil {
 			return nil, fmt.Errorf("error compiling regex %q", s)
 		}
 	}
 	return &KeyFilter{
-		blacklist: regexp.MustCompile("^(?:(?:" + strings.Join(blacklist, ")|(?:") + ")).*"),
+		rejectlist: regexp.MustCompile("^(?:(?:" + strings.Join(rejectlist, ")|(?:") + ")).*"),
 	}, nil
 }
 
@@ -32,8 +32,8 @@ func (kf KeyFilter) Keep(k string) bool {
 		// The Scoreboard shouldn't even send us these, but play it safe.
 		return false
 	}
-	if kf.blacklist == nil {
+	if kf.rejectlist == nil {
 		return true
 	}
-	return !kf.blacklist.MatchString(k)
+	return !kf.rejectlist.MatchString(k)
 }
